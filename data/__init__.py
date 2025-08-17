1205f7fab0f13dec36b32e3dd8475a613ae71c28
Karthik Iyer <karthik.iyer@hotmail.com>
2025-08-17 12:36:46 +1000

    Expose GraphDataset.from_parquet

--- data/__init__.py
+++ data/__init__.py
@@ -4,3 +4,10 @@ This package contains classes and functions to parse SMILES strings into
 graph representations, sample subgraphs for pretraining, and create
 batches of graphs for both unsupervised and supervised learning.
 """
+
+try:
+    from .mdataset import GraphDataset, GraphData  # type: ignore
+except Exception:  # pragma: no cover - optional dependency
+    GraphDataset = GraphData = None  # type: ignore
+
+__all__ = ["GraphDataset", "GraphData"]
